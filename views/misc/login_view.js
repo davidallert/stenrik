6625eb4451658cd36bdd3b728ba1cde03561f292
David A <david.allert@hotmail.com>
2024-07-22 23:16:48 +0200

    Add the start of a design to the login page.

--- views/misc/login_view.js
+++ views/misc/login_view.js
@@ -9,15 +9,26 @@ export default class LoginView extends HTMLElement {
 
     render() {
         this.innerHTML = `
-        <h1>Registrera ny användare</h1>
+        <h1>Logga in eller registrera en ny användare</h1>
+        <div class="double-buttons-wrapper">
+            <div class="image-div">
+                <div class="map-pin-circle"></div>
+            </div>
+            <div class="left-button">
+                <h2>Logga in</h2>
+            </div>
+            <div class="right-button">
+                <h2>Registrera</h2>
+            </div>
+        </div>
             <form>
-            <label for="username"></label>
-            <input type="text" name="username">
-            <label for="email"></label>
-            <input type="email" name="email">
-            <label for="password"></label>
-            <input type="password" name="password">
-            <input type="submit">
+                <label for="username">Användarnamn</label>
+                <input type="text" name="username">
+                <label for="email">E-post</label>
+                <input type="email" name="email">
+                <label for="password">Lösenord</label>
+                <input type="password" name="password">
+                <input type="submit">
             </form>
         `
     }
