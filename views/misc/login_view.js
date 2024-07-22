89175b8ad2f68d486c10a77bccd88fed22b99199
David A <david.allert@hotmail.com>
2024-07-23 00:34:16 +0200

    Add bobbing map pin and buttons to the login page.

--- views/misc/login_view.js
+++ views/misc/login_view.js
@@ -9,27 +9,33 @@ export default class LoginView extends HTMLElement {
 
     render() {
         this.innerHTML = `
-        <h1>Logga in eller registrera en ny användare</h1>
-        <div class="double-buttons-wrapper">
+        <div class="pin-and-buttons-wrapper">
+
             <div class="image-div">
                 <div class="map-pin-circle"></div>
             </div>
-            <div class="left-button">
-                <h2>Logga in</h2>
+
+            <div class="buttons-wrapper">
+                <div class="left-button">
+                    <h2 class="left-button-text noselect">Logga in</h2>
+                </div>
+                <div class="right-button">
+                    <h2 class="right-button-text noselect">Registrera</h2>
+                </div>
             </div>
-            <div class="right-button">
-                <h2>Registrera</h2>
-            </div>
+
         </div>
-            <form>
-                <label for="username">Användarnamn</label>
-                <input type="text" name="username">
-                <label for="email">E-post</label>
-                <input type="email" name="email">
-                <label for="password">Lösenord</label>
-                <input type="password" name="password">
-                <input type="submit">
-            </form>
+
         `
     }
 }
+
+{/* <form>
+<label for="username">Användarnamn</label>
+<input type="text" name="username">
+<label for="email">E-post</label>
+<input type="email" name="email">
+<label for="password">Lösenord</label>
+<input type="password" name="password">
+<input type="submit">
+</form> */}
