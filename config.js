5b7140801859c945997d94c568f31f1e991c38d3
David A <david.allert@hotmail.com>
2024-07-26 22:57:48 +0200

    Add public config file.

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+    supabaseUrl: 'https://tpjfocufiqqszdsrhfig.supabase.co',
+    supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwamZvY3VmaXFxc3pkc3JoZmlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE1MDUyMTcsImV4cCI6MjAzNzA4MTIxN30.ay6xXYLI6PPKgaSeJTYzShWkPbkWAfa3ImBwi0XQVcw'
+  };
+  
+  export default config;
