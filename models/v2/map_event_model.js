c14ae92263242a020e40b73c24ea991b1535ede7
David A <david.allert@hotmail.com>
2024-08-20 00:09:57 +0200

    Improve device orientation, fix bugs.

--- models/v2/map_event_model.js
+++ models/v2/map_event_model.js
@@ -48,7 +48,7 @@ const mapEventModel = {
                 const position = await locationModel.getInitialPosition();
                 locationModel.setCurrentPosition(position);
                 const locationMarkerIcon = L.divIcon({
-                    html: '<i class="fa-solid fa-location-arrow"></i>',
+                    html: '<i class="fa-solid fa-location-arrow fa-rotate-by" style="--fa-rotate-angle: -45deg;""></i>',
                     className: 'fa-location-icon',
                 });
 
