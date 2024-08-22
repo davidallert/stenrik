7a9759ca2aa4316b549f428c91b2e84166e85f6d
David A <david.allert@hotmail.com>
2024-08-22 11:37:30 +0200

    Add orientation value to popup.

--- models/v2/map_event_model.js
+++ models/v2/map_event_model.js
@@ -110,14 +110,14 @@ const mapEventModel = {
                     // The event will always trigger once when it's initialized. This happens on all devices.
                     // On desktop, it will never trigger twice.
                     // This will cause the dot icon to change to an arrow icon the first time it's triggered by an actual device orientation change.
-                    locationMarker.bindPopup(`<h3>Longitud: ${position.coords.longitude} | Latitud: ${position.coords.latitude}, Riktning: ${event.alpha}</h3>`);
+                    locationMarker.bindPopup(`<h3>Longitud: ${position.coords.longitude} | Latitud: ${position.coords.latitude}, Riktning: ${Math.ceil((360 - event.alpha) % 360)}</h3>`);
                     if (deviceOrientationTriggerIndex === 1) {
                         locationMarker.setIcon(locationMarkerIconArrow);
                     }
                     deviceOrientationTriggerIndex++;
 
                     let locationMarkerIconEl = document.getElementById("locationMarkerIconEl");
-                    adjustedRotation = 360 - event.alpha % 360;
+                    adjustedRotation = (360 - event.alpha) % 360;
                     locationMarkerIconEl.style.transform = `rotate(${adjustedRotation}deg)`;
                 });
 
