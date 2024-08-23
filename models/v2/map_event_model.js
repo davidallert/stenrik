d9fba351d80940cf2fa3b21ec9bb1f4666ae73ca
David A <david.allert@hotmail.com>
2024-08-23 12:51:34 +0200

    Attempt to add device orientation support for Safari.

--- models/v2/map_event_model.js
+++ models/v2/map_event_model.js
@@ -133,14 +133,14 @@ const mapEventModel = {
                             // The event will always trigger once when it's initialized. This happens on all devices.
                             // On desktop, it will never trigger twice.
                             // This will cause the dot icon to change to an arrow icon the first time it's triggered by an actual device orientation change.
-                            locationMarker.bindPopup(`<h3>Longitud: ${position.coords.longitude} | Latitud: ${position.coords.latitude}, Riktning: ${Math.ceil((360 - event.webkitCompassHeading) % 360)}</h3>`);
+                            locationMarker.bindPopup(`<h3>Longitud: ${position.coords.longitude} | Latitud: ${position.coords.latitude}, Riktning: ${Math.ceil(event.webkitCompassHeading - 45)}</h3>`);
                             if (deviceOrientationTriggerIndex === 1) {
                                 locationMarker.setIcon(locationMarkerIconArrow);
                             }
                             deviceOrientationTriggerIndex++;
     
                             let locationMarkerIconEl = document.getElementById("locationMarkerIconEl");
-                            adjustedRotation = (360 - event.webkitCompassHeading - 45) % 360;
+                            adjustedRotation = (event.webkitCompassHeading - 45);
                             locationMarkerIconEl.style.transform = `rotate(${adjustedRotation}deg)`;
                         });
                     }
