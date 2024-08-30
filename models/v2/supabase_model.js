2892ba7c6210d94d2defbec17565766f5f6cb2f2
David A <david.allert@hotmail.com>
2024-08-30 20:33:10 +0200

    Default map init data.

--- models/v2/supabase_model.js
+++ models/v2/supabase_model.js
@@ -16,8 +16,8 @@ const supabaseModel = {
         let { data: site_data, error } = await supabase
         .from('site_data')
         .select('id, site_id, raa_id, site_name, site_type, municipality, parish, province, county, desc_terrain, desc_orientation, desc_tradition, desc_text, coordinates')
-        // .eq('site_type', ['Runristning'])
-        .limit(100);
+        .eq('site_type', ['Runristning'])
+        .limit(1000);
         // .in('site_type', ['Kloster', 'Kyrka/kapell'])
 
         loading.removeSpinner();
